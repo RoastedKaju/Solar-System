730d9e6a1938c794cf20f18296d7333679fcc751
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2025-08-26 23:52:38 +0500

    create install_deps.bat

---
+++ install_deps.bat
@@ -0,0 +1,26 @@
+@echo off
+SETLOCAL
+
+REM setup vcpkg path
+set VCPKG_EXE=vendor\VCPKG\vcpkg.exe
+
+REM check if vcpkg exists
+if not exist %VCPKG_EXE% (
+	echo [ERROR] vcpkg.exe not found, please run "vendor/VCPKG/bootstrap-vcpkg.bat" first
+	pause
+	exit /B 1
+)
+
+REM install dependencies
+echo Installing dependencies....
+%VCPKG_EXE% install glfw3 glew fmt glm assimp
+
+if %errorlevel% neq 0 (
+	echo [ERROR] Installation failed.
+	pause
+	exit /B 1
+)
+
+echo [SUCCESS] Dependencies installed successfully
+ENDLOCAL
+PAUSE
